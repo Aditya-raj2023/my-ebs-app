6ef9cbff9b6d90a79e60754c4a88e473df760fc2
Aditya-raj2023 <adityaraj.rk56@gmail.com>
2026-04-22 00:18:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def home():
     return "Hello from my AWS App!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=8000, debug=False)
