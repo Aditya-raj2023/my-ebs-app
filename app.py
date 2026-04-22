b489ed5290a8ea49606508c331225b49dc4d98a2
Aditya-raj2023 <adityaraj.rk56@gmail.com>
2026-04-22 09:50:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello My name is Aditya welcome to my AWS App! is it working"
+    return "Hello My name is Aditya welcome to my AWS App! is it working updated"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=False)
